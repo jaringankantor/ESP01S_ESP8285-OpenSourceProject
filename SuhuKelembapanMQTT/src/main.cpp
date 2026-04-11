d2db24351f8ba1d3529e9435fabbc62ccfce21a6
nc.anwar@JaringanKantor.com <nc.anwar@jaringankantor.com>
2026-04-11 22:40:21 +0700

    Refactor code structure for improved readability and maintainability

--- SuhuKelembapanMQTT/src/main.cpp
+++ SuhuKelembapanMQTT/src/main.cpp
@@ -1,18 +1,54 @@
 #include <Arduino.h>
+#include "config.h"
+#include <ESP8266WiFi.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DHTPIN              2
+#define DHTTYPE             DHT11
+#define LED_PIN             2     // Built-in LED on GPIO2 (active-LOW)
+#define LED_BLINK_INTERVAL  100   // Fast blink interval (ms)
+
+DHT dht(DHTPIN, DHTTYPE);
+unsigned long previousLedMillis = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);  // LED off initially
+
+  dht.begin();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("\n\r \n\rWorking to connect");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nDHT Weather Reading Server");
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Fast blink 10x to indicate WiFi connected
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  unsigned long currentMillis = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (WiFi.status() == WL_CONNECTED) {
+    if (currentMillis - previousLedMillis >= LED_BLINK_INTERVAL) {
+      previousLedMillis = currentMillis;
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    }
+  } else {
+    digitalWrite(LED_PIN, HIGH);  // LED off when not connected
+  }
 }
